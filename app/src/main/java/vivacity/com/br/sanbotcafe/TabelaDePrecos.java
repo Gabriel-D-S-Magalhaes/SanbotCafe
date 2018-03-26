4c9a2f2f2890a6cb11a164eda215035497aaafcc
mac <vivacity.gabriel@outlook.com>
2018-03-26 18:40:27 -0300

    News TabelaDePrecos.java: Classe com constantes que armazenam os preços dos produtos Algumas mudanças nos layout

---
+++ app/src/main/java/vivacity/com/br/sanbotcafe/TabelaDePrecos.java
@@ -0,0 +1,19 @@
+package vivacity.com.br.sanbotcafe;
+
+/**
+ * Created by mac on 26/03/18.
+ *
+ * @author Gabriel Dos Santos Magalhães
+ */
+
+public class TabelaDePrecos {
+
+    public static final String TAG = TabelaDePrecos.class.getSimpleName();
+    public static final double VINHO = 3.50;
+    public static final double CERVEJA = 4.00;
+    public static final double CHA = 2.80;
+    public static final double CAFE = 3.50;
+    public static final double SUCO = 3.50;
+    public static final double AGUA = 2.00;
+    public static final double REFRIGERANTE = 2.50;
+}
